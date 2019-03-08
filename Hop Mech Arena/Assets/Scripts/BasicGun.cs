5bb53662587b44fe6dd77678b80fcf0633663c38
Nathan Holzworth <nholzwor@uncc.edu>
2019-03-08 15:40:02 -0500

    Initial Commit

---
+++ Hop Mech Arena/Assets/Scripts/BasicGun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicGun : MonoBehaviour
+{
+    public float refireWaitTime;
+    public float currentRefireWait;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public Vector3 projectileSpawnLoc;
+    public Vector3 projectileFireDir;
+    public GameObject playerCamera;
+    public GameObject cameraTarget;
+    public int playerNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNum = GetComponentInParent<PlayerCharacter>().playerNum;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentRefireWait--;
+    }
+
+    public void Fire()
+    {
+        if (currentRefireWait <= 0)
+        {
+                projectileSpawnLoc = transform.position;
+                projectileFireDir = Vector3.Normalize(cameraTarget.transform.position - projectileSpawnLoc);
+                Quaternion initialRotation = Quaternion.LookRotation(projectileFireDir);
+                GameObject newProjectile = Instantiate(projectile, projectileSpawnLoc, initialRotation);
+                newProjectile.GetComponent<BasicProjectile>().projectileDir = projectileFireDir;
+                newProjectile.GetComponent<BasicProjectile>().projectileSpeed = projectileSpeed;
+                newProjectile.GetComponent<BasicProjectile>().parentPlayerNum = playerNum;
+                Physics.IgnoreCollision(newProjectile.GetComponent<Collider>(), GetComponent<Collider>());
+                Physics.IgnoreCollision(newProjectile.GetComponent<Collider>(), GetComponentInParent<PlayerCharacter>().coll);
+                currentRefireWait = refireWaitTime;
+        }
+    }
+}
---
+++ Hop Mech Arena/Assets/Scripts/DebugSpeedometer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSpeedometer : MonoBehaviour
+{
+    public float averageSpeed;
+    public Vector3 averageVelocity;
+    public Vector3 startPos;
+    public Vector3 currentPos;
+    public Vector3 distanceTravelled;
+    public float timeFromStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentPos = transform.position;
+        distanceTravelled = currentPos - startPos;
+        averageVelocity = distanceTravelled / timeFromStart;
+        averageSpeed = averageVelocity.magnitude;
+        timeFromStart++;
+    }
+}
