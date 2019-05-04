7fb037131e5c26df8a288956927d431cfde10a13
Nathan Holzworth <thescud24@gmail.com>
2019-05-04 18:23:48 -0400

    Reimport with fixed gitignore

---
+++ Hop Mech Arena/Assets/Scripts/BasicProjectile.cs
@@ -0,0 +1,65 @@
+﻿/**
+    ITCS 4232-001 Group Project
+    BasicProjectile.cs
+    Purpose: 
+
+
+    @author Nathan Holzworth, (add your name here if you add stuff to this file)
+    @version 1.0 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicProjectile : MonoBehaviour
+{
+    public Vector3 projectileDir;
+    public float projectileSpeed;
+    public float timeToLive;
+    public float timeFromStart;
+    public float projectileGravityScale=1;//Disable UseGravity if using this value
+    public bool debugLoop;
+    protected Rigidbody rgbd;
+
+    public GameObject hitEffectObject;
+    public string projectileType;
+
+    public int parentPlayerNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rgbd.AddForce(projectileGravityScale * Physics.gravity * rgbd.mass);
+        Vector3 tempPos = transform.position;
+        tempPos += projectileDir * projectileSpeed;
+        rgbd.MovePosition(tempPos);
+
+        if (timeFromStart > timeToLive)
+        {
+            if (debugLoop)
+            {
+                timeFromStart = 0;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        timeFromStart++;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(projectileType == "Explosive")
+        {
+            Instantiate(hitEffectObject, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Hop Mech Arena/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿/**
+    ITCS 4232-001 Group Project
+    CameraController.cs
+    Purpose: 
+
+
+    @author Nathan Holzworth, (add your name here if you add stuff to this file)
+    @version 1.0 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraMoveTarget;
+    public Transform cameraLookTarget;
+    public Transform spectateCameraMoveTarget;
+    public Transform spectateCameraLookTarget;
+    public bool isSpectateMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isSpectateMode)
+        {
+            transform.position = spectateCameraMoveTarget.position;
+            transform.LookAt(spectateCameraLookTarget);
+        }
+        else
+        {
+            transform.position = cameraMoveTarget.position;
+            transform.LookAt(cameraLookTarget);
+        }
+    }
+}
