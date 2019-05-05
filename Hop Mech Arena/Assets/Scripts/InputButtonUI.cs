69b8fba5c5bb99ec55c7a5e40147fc25b715c94b
Nathan Holzworth <thescud24@gmail.com>
2019-05-04 21:03:05 -0400

    Setup player inputs and Controller Test Scene

---
+++ Hop Mech Arena/Assets/Scripts/InputButtonUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputButtonUI : MonoBehaviour
+{
+    public Image inputButtonFill;
+    public bool buttonOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color tempColor = inputButtonFill.color;
+        if (buttonOn)
+        {
+            tempColor.a = 255;
+        }
+        else
+        {
+            tempColor.a = 0;
+        }
+        inputButtonFill.color = tempColor;
+    }
+}
