8665946a4b4a69ed21f46277fc6305c6d0f3b180
Nathan Holzworth <thescud24@gmail.com>
2019-05-10 11:27:11 -0400

    Created player health manager

--- Hop Mech Arena/Assets/Scripts/IndependentAimPlayer.cs
+++ Hop Mech Arena/Assets/Scripts/IndependentAimPlayer.cs
@@ -42,7 +42,7 @@ public class IndependentAimPlayer : MonoBehaviour
 
     //public GameObject playerCollider;
     public bool isDummy = false;
-
+    public bool isDead;
     public BasicGun currentWeapon;
     Rigidbody rgbd;
     public Collider coll;
@@ -87,7 +87,7 @@ public class IndependentAimPlayer : MonoBehaviour
         {
             stunTimeRemaining--;
         }
-        if (!isDummy && !stunned)
+        if (!isDummy && !stunned && !isDead)
         {
             AimHandler();
 
---
+++ Hop Mech Arena/Assets/Scripts/PlayerStatsManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsManager : MonoBehaviour
+{
+    public float currentHealth;
+    public float MAX_HEALTH = 100;
+    public float currentShields;
+    public float MAX_SHIELDS;
+    public float currentShieldChargeDelay;
+    public float MAX_CHARGE_DELAY;
+    public float chargeRate;
+    public int currentLives;
+    public int currentRespawnTime;
+    public int MAX_RESPAWN_TIME;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(currentShieldChargeDelay <= 0)
+        {
+            if(currentShields < MAX_SHIELDS)
+            {
+                currentShields += chargeRate;
+                if(currentShields > MAX_SHIELDS)
+                {
+                    currentShields = MAX_SHIELDS;
+                }
+            }
+        }
+        else
+        {
+            currentShieldChargeDelay--;
+        }
+    }
+
+    public void ApplyDamage(float damageAmount)
+    {
+        currentShields -= damageAmount;
+        if (currentShields < 0)
+        {
+            currentHealth += currentShields;
+            currentShields = 0;
+            if(currentHealth <= 0)
+            {
+                HandleDeath();
+            }
+        }
+    }
+
+    public void ApplyHealing(float healingAmount)
+    {
+        currentHealth += healingAmount;
+        if(currentHealth > MAX_HEALTH)
+        {
+            currentHealth = MAX_HEALTH;
+        }
+    }
+
+    public void ApplyShieldCharge(float shieldAmount)
+    {
+        currentShields += shieldAmount;
+    }
+
+    public void HandleDeath()
+    {
+        currentLives--;
+        if (currentLives <= 0)
+        {
+            //Switch to spectate mode
+        }
+        else
+        {
+            currentRespawnTime = MAX_RESPAWN_TIME;
+            //Disable player control and hide model
+        }
+    }
+}
