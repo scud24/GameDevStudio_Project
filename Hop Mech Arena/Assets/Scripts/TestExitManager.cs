16d6565c77eea6155c00ff4669afa4ea089cd581
Nathan Holzworth <thescud24@gmail.com>
2019-05-12 11:28:25 -0400

    Added citations and cleaned up settings

---
+++ Hop Mech Arena/Assets/Scripts/TestExitManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestExitManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+    public void ReturnToMenuButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
